80677b7103934baae0b6934ec90b1f87d0f79498
EnmanConstruction <josh@enmanconstruction.com>
2025-04-15 11:38:31 -0600

    Update walkthrough.js
    Delete walkthrough.js (relocated to correct folder)

--- walkthrough.js
+++ walkthrough.js
@@ -1,8 +1,1 @@
-export default function Walkthrough() {
-  return (
-    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
-      <h1>🛠️ Walkthrough Step 1</h1>
-      <p>Welcome to the Enman Quote Tool walkthrough. Let's get started!</p>
-    </div>
-  );
-}
+
