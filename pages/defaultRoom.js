ec24ee303bab781fd27ce64daa286708b3b154de
EnmanConstruction <josh@enmanconstruction.com>
2025-04-21 19:41:28 -0600

    Update defaultRoom.js

--- pages/defaultRoom.js
+++ pages/defaultRoom.js
@@ -1,6 +1,7 @@
 const defaultRoom = {
   label: '',
   type: 'Bathroom',
+  date: '',
   length: '',
   width: '',
   photo: null,
@@ -17,9 +18,96 @@ const defaultRoom = {
     plumbing: true
   },
   flooring: { type: '', area: '' },
-  tile: { hasTubTile: false, hasBacksplash: false },
-  painting: { walls: false },
-  plumbing: { tub: false, toilet: false, sink: false }
+  tile: {
+    hasTubTile: false,
+    hasBacksplash: false,
+    edge: '',
+    tileColor: '',
+    edgeColor: '',
+    edgeSize: '',
+    grout: '',
+    groutSealer: false
+  },
+  painting: {
+    ceiling: false,
+    walls: false,
+    baseCase: false,
+    cabinets: false,
+    sealerRequired: false
+  },
+  baseAndCase: {
+    paint: false,
+    replace: false,
+    linearFeet: '',
+    material: ''
+  },
+  drywall: {
+    ceilingType: '',
+    drywallPatches: false,
+    moldDrywall: false,
+    insulation: false,
+    deleteIntercom: false,
+    backingRequired: false,
+    cornerBead: false,
+    greenBoard: false,
+    fullReplacement: false,
+    patchCount: '',
+    ceilingDrywallType: '',
+    wallDrywallType: ''
+  },
+  electrical: {
+    outlets: '',
+    switches: '',
+    switchType: '',
+    smokeDetector: false,
+    gfci: '',
+    lightFixtures: '',
+    exhaustFan: false,
+    fanLocation: '',
+    panelUpgradeRequired: false,
+    fixtureHeights: '',
+    arcFaultBreakers: false
+  },
+  cabinets: {
+    upperQty: '',
+    lowerQty: '',
+    linearFeet: '',
+    gableEnds: '',
+    vanityQty: '',
+    vanitySize: '',
+    lazySusans: '',
+    cornerUnits: '',
+    drawerBanks: '',
+    finishType: '',
+    hardwareType: '',
+    softClose: false
+  },
+  countertops: {
+    sqft: '',
+    type: '',
+    edgeStyle: '',
+    backsplashHeight: '',
+    color: ''
+  },
+  plumbing: {
+    tub: false,
+    tubDirection: '',
+    tubSize: '',
+    showerRod: false,
+    toilet: false,
+    sink: false,
+    absFittings: false,
+    copperPipe: false,
+    shutOffs: false,
+    absPipe: false,
+    pTrap: false,
+    pTrapCleanout: false,
+    plumbingNotes: '',
+    showerValve: false,
+    supplyLines: false,
+    newDrain: false,
+    ventingRequired: false
+  }
 };
 
 export default defaultRoom;
